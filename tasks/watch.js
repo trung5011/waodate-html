b5d215e277df998438d4d9b80b92c8bfb411a544
Nguyen Sang <nnsang24@gmail.com>
2019-06-19 22:37:07 +0700

    add block page

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,7 @@
 module.exports = function (gulp, $, browserSync) {
 	gulp.task('watch', function () {
 		gulp.watch(['./src/styles/**/*.scss', './src/lib/**/*.scss'], ['inject-sass','tao-sass']);
-		gulp.watch('./src/img/**/*.*', ['copy-img']);
+		gulp.watch('./src/img/**/*', ['copy-img']);
 		gulp.watch(['./src/templates/**/*.pug', './src/lib/**/*.pug'], ['tao-html']);
 		gulp.watch('./src/scripts/**/*.js', ['tao-js']);
 		gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
